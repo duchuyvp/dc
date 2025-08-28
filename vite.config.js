bc887d8f20ba7dc7c04474daaa0f19854c1224ea
default <me@lunalovegood.dev>
2025-08-28 10:17:16 +0000

    .

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: ['5173--main--white-tarantula-30--huy.lunalovegood.dev']
+  }
 })
